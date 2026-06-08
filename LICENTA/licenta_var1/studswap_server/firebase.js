198db1d5da4a01e566d34f35872471c48ae34d6c
iarina petra <91983224+k4tt0@users.noreply.github.com>
2026-06-08 20:52:25 +0300

    added proj files

---
+++ LICENTA/licenta_var1/studswap_server/firebase.js
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./firebaseServiceAccount.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+const auth = admin.auth();
+
+module.exports = { db, auth };
