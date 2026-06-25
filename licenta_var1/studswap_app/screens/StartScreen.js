3b029b036eda7253605ef8a8b4e0c1ff3566bdac
iarina petra <91983224+k4tt0@users.noreply.github.com>
2026-06-26 00:20:18 +0300

    refactor: Remove theme toggle functionality and clean up StartScreen component

--- licenta_var1/studswap_app/screens/StartScreen.js
+++ licenta_var1/studswap_app/screens/StartScreen.js
@@ -2,30 +2,15 @@ import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-// import { colors } from '../styles/RegisterScreenStyle';
-// import { styles } from '../styles/StartPageStyle'; 
-
 import { useTheme } from '../context/ThemeContext';
 import { getStartStyles } from '../styles/StartPageStyle';
 
 export default function StartScreen({ navigation }) {
-  const { colors, isDarkMode, updateTheme } = useTheme();
+  const { colors } = useTheme();
   const styles = getStartStyles(colors);
 
   return (
     <View style={styles.container}>
-      
-      <TouchableOpacity 
-        style={styles.themeToggle}
-        onPress={() => updateTheme(isDarkMode ? 'light' : 'dark')}
-        activeOpacity={0.7}
-      >
-        <MaterialCommunityIcons 
-          name={isDarkMode ? "weather-sunny" : "weather-night"} 
-          size={24} 
-          color={colors.textDark} 
-        />
-      </TouchableOpacity>
 
       <View style={styles.centerContent}>
         <MaterialCommunityIcons name="school-outline" size={100} color={colors.textDark} />
@@ -38,7 +23,7 @@ export default function StartScreen({ navigation }) {
           onPress={() => navigation.navigate('Login')}
           activeOpacity={0.7}
         >
-          <Text style={[styles.buttonTextDark, { color: colors.textDark }]} numberOfLines={1} >Log In</Text>
+          <Text style={[styles.buttonTextDark, { color: colors.textDark }]} numberOfLines={1}>Log In</Text>
         </TouchableOpacity>
 
         <TouchableOpacity 
@@ -53,34 +38,3 @@ export default function StartScreen({ navigation }) {
     </View>
   );
 }
-
-// export default function StartScreen({ navigation }) {
-//   return (
-//     <View style={styles.container}>
-      
-//       <View style={styles.centerContent}>
-//         <MaterialCommunityIcons name="school-outline" size={100} color={colors.textDark} />
-//         <Text style={styles.title}>Welcome to StudSwap!</Text>
-//       </View>
-
-//       <View style={styles.buttonContainer}>
-//         <TouchableOpacity 
-//           style={styles.buttonOutline} 
-//           onPress={() => navigation.navigate('Login')}
-//           activeOpacity={0.7}
-//         >
-//           <Text style={styles.buttonTextDark}>Log In</Text>
-//         </TouchableOpacity>
-
-//         <TouchableOpacity 
-//           style={styles.buttonSolid} 
-//           onPress={() => navigation.navigate('Register')}
-//           activeOpacity={0.7}
-//         >
-//           <Text style={styles.buttonTextDark}>Sign Up</Text>
-//         </TouchableOpacity>
-//       </View>
-
-//     </View>
-//   );
-// }
